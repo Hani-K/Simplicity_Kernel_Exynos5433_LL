3200ef44aca128ae96edb576282bbb9b0c8557d1
Hani-K <h4ni12@gmail.com>
2016-01-27 02:49:03 +0500

    3.10.95

--- net/bluetooth/sco.c
+++ net/bluetooth/sco.c
@@ -448,7 +448,7 @@ static int sco_sock_create(struct net *net, struct socket *sock, int protocol,
 	return 0;
 }
 
-static int sco_sock_bind(struct socket *sock, struct sockaddr *addr, int alen)
+static int sco_sock_bind(struct socket *sock, struct sockaddr *addr, int alen, int addr_len)
 {
 	struct sockaddr_sco sa;
 	struct sock *sk = sock->sk;
@@ -462,6 +462,9 @@ static int sco_sock_bind(struct socket *sock, struct sockaddr *addr, int alen)
 	memset(&sa, 0, sizeof(sa));
 	len = min_t(unsigned int, sizeof(sa), alen);
 	memcpy(&sa, addr, len);
+
+	if (addr_len < sizeof(struct sockaddr_sco))
+		return -EINVAL;
 
 	lock_sock(sk);
 
